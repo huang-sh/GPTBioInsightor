605aeb58772f2151cbde35f725b0f719856b3b9f
shuang <shenghui.huang@unito.it>
2024-07-27 19:24:34 +0000

    divide output md into two md files

--- gptbioinsightor/celltype.py
+++ gptbioinsightor/celltype.py
@@ -1,4 +1,5 @@
 import os, sys
+from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor
 
 import scanpy as sc
@@ -42,10 +43,15 @@ def gptcelltype(
     for k in gene_dic.keys():
             gene_dic[k] = gene_dic[k][:topgenes]
     if out is None:
-        outhandle = sys.stdout
+        likely_handle, most_handle = sys.stdout, sys.stdout
     else:
-        outhandle = open(out, "w")
-        print("# All possible celltypes", file=outhandle)
+        out = Path(out)
+        likely_path = out.with_suffix(f".likely{out.suffix}")
+        most_path = out.with_suffix(f".most{out.suffix}")
+        likely_handle = open(likely_path, "w")
+        most_handle = open(most_path, "w")
+        print("# All possible celltypes", file=likely_handle)
+        print("\n\n# Most Possible celltypes", file=most_handle)
         
     if n_jobs is None:
         n_jobs = min(os.cpu_count()//2, len(gene_dic))
@@ -53,23 +59,32 @@ def gptcelltype(
     def _aux_func(item):
         return _query_celltype(item[1][:topgenes], item[0], background, provider, model, base_url, sys_prompt)
         
-    text = ""
+    likely_res_ls = []
     with ThreadPoolExecutor(max_workers=n_jobs) as executor:
         results = executor.map(_aux_func, gene_dic.items())
         for (gsid, genes), res in zip(gene_dic.items(), results):
             res = res.strip("```").strip("'''")
-            print(res, file=outhandle)
-            text += res
+            print(res, file=likely_handle)
+            likely_res_ls.append(res)
 
-    msgs = [{"role": "user", "content": text},
-            {"role": "user", "content": FINAL_CELLTYPE_PROMPT.format(geneset_num=len(gene_dic), background=background)}]
-    response = query_model(msgs, provider=provider, model=model, base_url=base_url, sys_prompt=sys_prompt)
-    print("\n\n# Most Possible celltypes", file=outhandle)
-    res_content = response.choices[0].message.content.strip("```").strip("'''")
-    print(res_content, file=outhandle)
+    celltype_ls = []
+    for i in range(0, len(likely_res_ls), 5):
+        part_likely_res = "".join(likely_res_ls[i:i + 5])
+        msgs = [
+            {"role": "user", "content": part_likely_res},
+            {"role": "user", "content": FINAL_CELLTYPE_PROMPT.format(geneset_num=len(likely_res_ls[i:i + 5]), background=background)}
+        ]
+
+        response = query_model(msgs, provider=provider, model=model, base_url=base_url, sys_prompt=sys_prompt)
+        
+        res_content = response.choices[0].message.content.strip("```").strip("'''")
+        print(res_content, file=most_handle)
+        celltype_ls.extend([line.split(":")[1].strip() for line in res_content.split("\n") if line.startswith("###")])
+
     if out is not None: 
-        outhandle.close()
-    celltypes = [line.split(":")[1].strip() for line in res_content.split("\n") if line.startswith("###")]
-    celltype_dic = {k:celltypes[idx] for idx, k in enumerate(gene_dic.keys())}
+        likely_handle.close()
+        most_handle.close()
+        
+    celltype_dic = {k:celltype_ls[idx] for idx, k in enumerate(gene_dic.keys())}
     return celltype_dic
 
