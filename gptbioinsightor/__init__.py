11531bfe9a03beeffec2d0947f34deca8fc26864
shuang <shenghui.huang@unito.it>
2024-08-25 05:12:36 +0000

    update

--- gptbioinsightor/__init__.py
+++ gptbioinsightor/__init__.py
@@ -1,5 +1,6 @@
 from .celltype import get_celltype, get_subtype, check_celltype
+from .pathway import depict_pathway
 from .utils import get_marker_from_seurat
 
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
