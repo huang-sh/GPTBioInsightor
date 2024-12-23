8ef8b1304aac7658753134925ea0a8d0f6bb9c30
shuang <shenghui.huang@unito.it>
2024-12-23 19:03:43 +0000

    add use_cache test

--- tests/test_agent_cls.py
+++ tests/test_agent_cls.py
@@ -7,7 +7,15 @@ MODEL_LIST = pytest.MODEL_LIST
 
 def test_agent(provider, model):
     agent = Agent(model=model, provider=provider, sys_prompt=None, base_url=None)
-    response = agent.query("what is your modes", add_context=False)
+    
+    response_any1 = agent.query("tell me something", add_context=False)
+    response_any2 = agent.query("tell me something", add_context=False)
+    assert response_any1 == response_any2
+
+    response_m1 = agent.query("what is your models", add_context=False,use_cache=False)
+    response_m2 = agent.query("what is your models", add_context=False,use_cache=False)
+    assert response_m1 != response_m2
+
     response1 = agent.query("Please remember I am Bob")
     response2 = agent.query("what is my name?")
     assert len(agent.history) == 4
