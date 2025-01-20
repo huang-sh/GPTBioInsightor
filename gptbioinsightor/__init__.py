e827b47488f68b60c3640910940e0ca28e46b830
shuang <shenghui.huang@unito.it>
2025-01-20 07:14:20 +0000

    update

--- gptbioinsightor/__init__.py
+++ gptbioinsightor/__init__.py
@@ -1,7 +1,10 @@
 from .celltype import get_celltype, get_subtype, check_celltype
-from .pathway import depict_pathway
-from .core import query_model
-from .utils import get_marker_from_seurat, set_api_key
+from .pathway import depict_pathway, name_pathway, analyse_pathway
+from .core import query_model, Agent
+from .gsea import enrich
+from .prompt import *
+from . import utils
+from .utils import get_marker_from_seurat, set_api_key, list_celltype, score_heatmap, add_obs
 
 
-__version__ = "0.6.1"
+__version__ = "0.7.0a1"
